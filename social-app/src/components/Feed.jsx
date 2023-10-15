bfa12b1184e3e228b7587357ec812fef7ad812b3
ltr <apuroopgupca007@gmail.com>
2023-10-15 19:04:00 +0530

    Developed Feed and Post components, Positioned SideBar and Rightbar as Fixed

--- social-app/src/components/Feed.jsx
+++ social-app/src/components/Feed.jsx
@@ -1,12 +1,16 @@
-import { Box } from '@mui/material'
-import React from 'react'
+import { Box } from "@mui/material";
+import Post from "./Post";
 
 const Feed = () => {
   return (
-    <Box bgcolor="pink" flex={4} p={2} sx={{
-  
-    }}>Feed</Box>
-  )
-}
+    <Box flex={4} px={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
+};
 
-export default Feed
+export default Feed;
