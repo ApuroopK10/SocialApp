f3f65bf9729c07fb420857442cf2270a626d9b7c
ltr <apuroopgupca007@gmail.com>
2023-10-15 19:28:57 +0530

    Added RightBar component with dummy data

--- social-app/src/components/RightBar.jsx
+++ social-app/src/components/RightBar.jsx
@@ -1,4 +1,16 @@
-import { Box } from "@mui/material";
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Divider,
+  ImageList,
+  ImageListItem,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const RightBar = () => {
@@ -13,7 +25,138 @@ const RightBar = () => {
         },
       }}
     >
-      <Box position="fixed">RightBar</Box>
+      <Box position="fixed" width={300} m="auto">
+        <Typography variant="h6" fontWeight={100}>
+          Online Friends
+        </Typography>
+        <AvatarGroup max={4}>
+          <Avatar
+            alt="Remy Sharp"
+            src="https://images.pexels.com/photos/3755021/pexels-photo-3755021.jpeg?auto=compress&cs=tinysrgb&w=600"
+          />
+          <Avatar
+            alt="Travis Howard"
+            src="https://images.pexels.com/photos/4668553/pexels-photo-4668553.jpeg?auto=compress&cs=tinysrgb&w=600"
+          />
+          <Avatar
+            alt="Cindy Baker"
+            src="https://images.pexels.com/photos/4668513/pexels-photo-4668513.jpeg?auto=compress&cs=tinysrgb&w=600"
+          />
+          <Avatar
+            alt="Agnes Walker"
+            src="https://images.pexels.com/photos/2362002/pexels-photo-2362002.jpeg?auto=compress&cs=tinysrgb&w=600"
+          />
+          <Avatar alt="Trevor Henderson" />
+        </AvatarGroup>
+        <Typography variant="h6" fontWeight={100} mt={2} mb={2}>
+          Latest Photos
+        </Typography>
+        <ImageList cols={3} rowHeight={100} gap={5}>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/4668509/pexels-photo-4668509.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="ImageListItem"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/1266741/pexels-photo-1266741.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="ImageListItem"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/1261572/pexels-photo-1261572.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="ImageListItem"
+              loading="lazy"
+            />
+          </ImageListItem>
+          <ImageListItem>
+            <img
+              src="https://images.pexels.com/photos/2825749/pexels-photo-2825749.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt="ImageListItem"
+              loading="lazy"
+            />
+          </ImageListItem>
+        </ImageList>
+        <Typography variant="h6" fontWeight={100} mt={2}>
+          Latest Chats
+        </Typography>
+        <List
+          sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}
+        >
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar
+                alt="Remy Sharp"
+                src="https://images.pexels.com/photos/3755021/pexels-photo-3755021.jpeg?auto=compress&cs=tinysrgb&w=600"
+              />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Brunch this weekend?"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Ali Connors
+                  </Typography>
+                  {" — I'll be in your neighborhood doing errands this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Summer BBQ"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    to Scott, Alex, Jennifer
+                  </Typography>
+                  {" — Wish I could come, but I'm out of town this…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+          <Divider variant="inset" component="li" />
+          <ListItem alignItems="flex-start">
+            <ListItemAvatar>
+              <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
+            </ListItemAvatar>
+            <ListItemText
+              primary="Oui Oui"
+              secondary={
+                <React.Fragment>
+                  <Typography
+                    sx={{ display: "inline" }}
+                    component="span"
+                    variant="body2"
+                    color="text.primary"
+                  >
+                    Sandra Adams
+                  </Typography>
+                  {" — Do you have Paris recommendations? Have you ever…"}
+                </React.Fragment>
+              }
+            />
+          </ListItem>
+        </List>
+      </Box>
     </Box>
   );
 };
